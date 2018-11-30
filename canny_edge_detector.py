9918e5f2edcf81ca0fe33896307b6395a314af7f
Cornelius <cornelius@corneliuseinem.com>
2018-11-30 16:42:24 +0100

    Update canny_edge_detector.py
    Added Non-Maximum Suppression and image output

--- canny_edge_detector.py
+++ canny_edge_detector.py
@@ -8,7 +8,7 @@ from scipy import *
 from scipy.ndimage import *
 
 # Import Image
-im = scipy.misc.imread('eurowings.jpg', mode='F')
+im = scipy.misc.imread('input_image.png', mode='F')
 
 # Plot Imported Image and find out dimensions
 # print(im.shape)
@@ -47,3 +47,31 @@ threshold_Edges = (gradient_magnitude > threshold)
 
 theta = theta * 180 / np.pi
 theta = np.absolute(around(theta / 45.0) * 45.0)
+
+# Apply Non - Maximum Suppression
+h, w = theta.shape[:2]
+# Initialize the Edge Detection as Black, so no Edges
+Edges = np.zeros(theta.shape)
+for y in range(1, h-2):
+    for x in range(1, w-2):
+        # Each time the gradient reaches above a threshold, check for the gradient direction and add the appropriate Edge to the finished Image
+        if threshold_Edges[y, x] == 1:
+            if (theta[y, x] == 0) | (theta[y, x] == 180):
+                if (gradient_magnitude[y, x] >= gradient_magnitude[y, x-1]) & (gradient_magnitude[y, x] >= gradient_magnitude[y, x+1]):
+                    Edges[y, x] = 1
+            elif theta[y, x] == 45:
+                if (gradient_magnitude[y, x] >= gradient_magnitude[y-1, x+1]) & (gradient_magnitude[y, x] >= gradient_magnitude[y+1, x-1]):
+                    Edges[y, x] = 1
+            elif theta[y, x] == 90:
+                if (gradient_magnitude[y, x] >= gradient_magnitude[y-1, x]) & (gradient_magnitude[y, x] >= gradient_magnitude[y+1, x]):
+                    Edges[y, x] = 1
+            elif theta[y, x] == 135:
+                if (gradient_magnitude[y, x] >= gradient_magnitude[y-1, x-1]) & (gradient_magnitude[y, x] >= gradient_magnitude[y+1, x+1]):
+                    Edges[y, x] = 1
+            else:
+                print("ERROR")
+                print(theta[y, x])
+
+
+# Save Detected Edges as new image
+scipy.misc.imsave('canny_edges.png', Edges)
